01d1c718fa85256dfb5bcad1e83dc9a685d67a8b
k4neka <artur.k200427@gmail.com>
2025-10-22 00:14:12 +0100

    Remove .history folder from tracking and update gitignore

--- .history/refract-minigame/game/entities/__init___20251021220940.py
+++
@@ -1,6 +0,0 @@
-"""Game entities."""
-from .player import Player
-from .projectile import Projectile
-from .collision_effect import CollisionEffect
-
-__all__ = ['Player', 'Projectile', 'CollisionEffect']
